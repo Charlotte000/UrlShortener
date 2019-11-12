b2b9c4a3406d773cde7c85ebed7af81c154059c8
Charlotte000 <ramil.mifteev@yandex.ru>
2019-11-12 22:07:27 +0300

    heroku deploy

--- settings.js
+++ settings.js
@@ -1,3 +1,9 @@
+settings = {
+	login: LOGIN,
+	password: PASSWORD,
+	dbName: DBNAME
+}
+
 settings.url = `mongodb+srv://${settings.login}:${settings.password}@cluster0-ji8lc.gcp.mongodb.net/test?retryWrites=true&w=majority`
 
 module.exports = settings;
